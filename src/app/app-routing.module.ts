034ed2c1cf78cfdc43370a7bad7b98ada8d6864e
LiKoloz <Ptvkzr_YbrekbyfH@mail.ru>
2022-08-21 17:56:01 +0300

    Add registrationPage and sig-inPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegistrationComponent} from "./components/registration/registration.component";
+import {AppComponent} from "./app.component";
+import {LoginComponent} from "./components/login/login.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'main-comp',
+  template: `
+    <router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  title = 'social-Network-V-2.0';
 }
