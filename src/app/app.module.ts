5724fd701aabf6ca01629ebdaaa2f2f31558e075
LiKoloz <Ptvkzr_YbrekbyfH@mail.ru>
2022-08-22 12:53:08 +0300

    login_cheсker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  loginForm!:FormGroup
+  constructor() {
+  }
+  submitLogin(){
+    console.log(this.loginForm.value)
+  }
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('',[Validators.required, Validators.email]),
+      'password': new FormControl('',
+        [Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)])
+    })
   }
 
 }
